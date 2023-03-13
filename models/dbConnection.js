f5de6d77994fb763504093d2f8a53c9d1486dfb3
Durgesh Maurya <durgesh.maurya0998@gmail.com>
2023-03-13 23:16:12 +0530

    cloude db connections

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 // mongodb database connections 
 
-mongoose.connect('mongodb://admin:admin@localhost:27017/customer?authSource=admin',{useNewUrlParser:true,useUnifiedTopology:true})
+// mongoose.connect('mongodb://admin:admin@localhost:27017/customer?authSource=admin',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://admin:admin@customer.nwim3y6.mongodb.net/customer?authSource=admin',{useNewUrlParser:true,useUnifiedTopology:true})
 .then((res)=>{
     console.log("connection success");
 })
